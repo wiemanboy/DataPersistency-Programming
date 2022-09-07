6616be6324d2163fb66f2a3b1a0e9451c5c124be
Jarno <wiemanboy@gmail.com>
2022-09-07 15:58:21 +0200

    P1 gemaakt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,34 +1,64 @@
 import java.sql.*;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public static Connection getConnection() {
         try {
             Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost/ovchip", "postgres", "new_password");
-            Statement statement = connection.createStatement();
-
-            String q = "select * from reiziger";
-            PreparedStatement pst = connection.prepareStatement(q);
-            ResultSet result = pst.executeQuery();
-
-
-            System.out.println("Alle Reizigers:");
-            while (result.next()) {
-                String tussenvoegsel = result.getString("tussenvoegsel");
-                if (tussenvoegsel == null) {
-                    tussenvoegsel = "";
-                }
-
-                System.out.println(
-                        "\t#" + result.getString("reiziger_id") + " " +
-                                result.getString("voorletters") + ". " +
-                                tussenvoegsel + " " +
-                                result.getString("achternaam") + " " +
-                                "(" + result.getString("geboortedatum") + ")"
-                );
-            }
-            connection.close();
+            return connection;
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        catch (Exception e) {e.printStackTrace();}
+        return null;
+    }
+
+    public static void printReiziger(Connection connection) {
+        if (connection != null) {
+            try {
+                Statement statement = connection.createStatement();
+
+                // get reizigers
+                String q = "select * from reiziger";
+                PreparedStatement pst = connection.prepareStatement(q);
+                ResultSet result = pst.executeQuery();
+
+                // print all reizigers
+                System.out.println("Alle Reizigers:");
+                while (result.next()) {
+                    // check tussenvoegsel for null
+                    String tussenvoegsel = result.getString("tussenvoegsel");
+                    if (tussenvoegsel == null) {
+                        tussenvoegsel = "";
+                    }
+
+                    // print info
+                    System.out.println(
+                            "\t#" + result.getString("reiziger_id") + " " +
+                                    result.getString("voorletters") + ". " +
+                                    tussenvoegsel + " " +
+                                    result.getString("achternaam") + " " +
+                                    "(" + result.getString("geboortedatum") + ")"
+                    );
+                }
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Connection connection = getConnection();
+        printReiziger(connection);
+        closeConnection(connection);
     }
 }
